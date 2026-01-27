2a2b61ef14ef3caf228aa61b164f81fd4a99e013
renelps <renanlps616@gmail.com>
2026-01-27 12:59:22 -0300

    refactor: simplify cloudinary configuration to use single connection string

--- src/main/java/com/example/auratechApi/config/CloudinaryConfig.java
+++ src/main/java/com/example/auratechApi/config/CloudinaryConfig.java
@@ -11,11 +11,6 @@ import java.util.Map;
 @Configuration
 public class CloudinaryConfig {
 
-    @Value("${cloudinary.cloud-name}")
-    private String cloudName;
-
-    @Value("${cloudinary.api-key}")
-    private String apiKey;
 
     @Value("${cloudinary.url}")
     private String cloudinaryUrl;
